742c187902605fc2a2b1a08a24f03c5853461aa5
wangxinyu <shenzhenwangxy@sina.com>
2024-11-27 07:04:52 +0800

    第二次提交

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,31 +3,148 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control"></tab-control>
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import { getMultiData } from "network/home";
+import { getMultiData,getProductData } from "network/home";
+import {TOP_DISTANCE, POP, NEW, SELL} from "common/const";
 import HomeSwiper from "./childComps/HomeSwiper";
-import RecommendView from './childComps/RecommendView.vue';
+import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView";
+import TabControl from "components/content/tabControl/TabControl";
 export default {
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView,
+    TabControl,
   },
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 1, list: [] },
+        new: { page: 1, list: [] },
+        sell: { page: 1, list: [] },
+      },
     };
   },
   created() {
     // 1.请求轮播等数据
-    getMultiData().then((data) => {
-      this._getMultiData();
-    });
+    this._getMultiData();
+
+    // 2.请求商品数据
+    // this._getProductData(POP);
+    // this._getProductData(NEW);
+    // this._getProductData(SELL);
   },
   methods: {
     /**
@@ -39,15 +156,37 @@ export default {
         this.recommends = res.data.recommend.list;
       });
     },
+    _getProductData(type) {
+      // 获取页码
+      const page = this.goods[type].page;
+      getProductData(type, page).then((res) => {
+        const newList = res.data.list;
+        this.goods[type].list.push(...newList);
+        this.goods[type].page += 1;
+
+        // 完成加载更多数据
+        this.$refs.scroll.finishedPullUp();
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
-  position: relative;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
   z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
